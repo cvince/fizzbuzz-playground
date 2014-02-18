68111a61790fd99e05933ad4ba7846da8a84b5e3
Vincent Chan <vincent@vincentcchan.com>
2014-02-18 11:30:09 -0800

    fizz buzz initial commit

---
+++ FizzBuzz.js
@@ -0,0 +1,26 @@
+//fizzbuzz extended
+
+
+/*
+
+  num : number to count to;
+  ext : numbers for which to trigger fizzbuzzXXX
+  msg : message to output when ext triggered
+
+*/
+
+
+var refs = {
+  3: "Fizz",
+  5: "Buzz",
+  9: "Sivv",
+  11: "Clutch",
+}
+
+var fizzBuzzArr = function(max, refs){
+
+  for(var i = 1; i<=max; i++){
+    Object.keys(refs);
+  }
+
+}
